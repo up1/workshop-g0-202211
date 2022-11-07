d07bf6e5d8c991307310cc441db0844959673b7c
up1 <somkiat.p@gmail.com>
2022-11-07 16:19:14 +0700

    Working with struct and interface

--- demo-workspace/module_b/customer.go
+++ demo-workspace/module_b/customer.go
@@ -1,11 +1,25 @@
 package module_b
 
+type ICustomer interface {
+	SayHi() string
+}
+
 type Customer struct{
 	Id int
 	Name string
 }
 
+type CustomerV2 struct{
+	Age int
+	Customer
+}
+
 func NewCustomer() Customer {
+	c2 := CustomerV2{}
+	c2.Id = 1
+	c2.Name = "xxx"
+	c2.Age = 10
+
 	return Customer{}
 }
 
