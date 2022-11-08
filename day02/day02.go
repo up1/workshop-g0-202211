3924b6d20ea7cafa9d97bbaf7ab90b77bdae2289
up1 <somkiat.p@gmail.com>
2022-11-08 09:55:15 +0700

    Add to string

--- day02/day02.go
+++ day02/day02.go
@@ -1,7 +1,13 @@
 package day02
 
+import "fmt"
+
 type Hello struct{
 	Name string
+}
+
+func (h Hello) String() string {
+	return fmt.Sprintf("Name=%s", h.Name)
 }
 
 func NewHello(name string) Hello {	
