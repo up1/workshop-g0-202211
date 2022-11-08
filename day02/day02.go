d129bda9eb0ca5a56dbe14df4979827a6f861a7b
up1 <somkiat.p@gmail.com>
2022-11-08 10:23:51 +0700

    Working with go doc

--- day02/day02.go
+++ day02/day02.go
@@ -2,18 +2,22 @@ package day02
 
 import "fmt"
 
+// Hello struct
 type Hello struct{
 	Name string
 }
 
+// To String
 func (h Hello) String() string {
 	return fmt.Sprintf("Name=%s", h.Name)
 }
 
+// Create new Hello
 func NewHello(name string) Hello {	
 	return Hello{ Name: name }
 }
 
+// SayHi with name
 func (h Hello) SayHi() string {
 	return "Hello " + h.Name
 }
--- day02/day02_test.go
+++ day02/day02_test.go
@@ -3,12 +3,12 @@ package day02_test
 import (
 	"day02"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestHelloWithName(t *testing.T) {
 	h := day02.NewHello("somkiat")
 	got := h.SayHi()
-	if got != "Hello somkiat" {
-		t.Errorf("Error mismatch with %s", got)
-	}
+	assert.Equal(t, "Hello somkiat", got)
 }
