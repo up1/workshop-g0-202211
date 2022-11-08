80ac19f932b8c6205f99644237eb4f84dc952a0d
up1 <somkiat.p@gmail.com>
2022-11-08 15:24:40 +0700

    Workshop with waitgroup

--- day02/cmd/demo_routine_2.go
+++ day02/cmd/demo_routine_2.go
@@ -4,16 +4,23 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"sync"
 )
 
-// var wg sync.WaitGroup
+var wg sync.WaitGroup
 
 func main() {
-	_, err := fetch("https://jsonplaceholder.typicode.com/users")
-	fmt.Println(err)
+	wg.Add(1)
+	go fetch("https://www.google.com")
+
+	wg.Add(1)
+	go fetch("https://jsonplaceholder.typicode.com/users", )
+
+	wg.Wait()
 }
 
 func fetch(url string) (string, error) {
+	defer wg.Done()
 	// Use package net/http
 	resp, err := http.Get(url)
 	if err != nil {
@@ -21,5 +28,6 @@ func fetch(url string) (string, error) {
 	}
 	defer resp.Body.Close()
 	body, err := io.ReadAll(resp.Body)
+	fmt.Println("Done with ", url)
 	return string(body), nil
 }
