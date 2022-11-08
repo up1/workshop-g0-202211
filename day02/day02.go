4d7347363ee05451cc731ad4d5e4027b4d0100a9
up1 <somkiat.p@gmail.com>
2022-11-08 09:49:32 +0700

    Create first case

--- day02/day02.go
+++ day02/day02.go
@@ -1,2 +1,9 @@
 package day02
 
+type Hello struct{
+	Name string
+}
+
+func NewHello(name string) Hello {	
+	return Hello{ Name: name }
+}
--- day02/day02_test.go
+++ day02/day02_test.go
@@ -1,1 +1,12 @@
 package day02_test
+
+import (
+	"day02"
+	"fmt"
+	"testing"
+)
+
+func TestHelloWithName(t *testing.T) {
+	h := day02.NewHello("somkiat")
+	fmt.Println(h)
+}
