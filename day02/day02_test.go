b529cf455f21879d7f5ed83a52b5c220da095a79
up1 <somkiat.p@gmail.com>
2022-11-08 11:16:14 +0700

    Add example and beanchmark

--- day02/day02_test.go
+++ day02/day02_test.go
@@ -2,6 +2,7 @@ package day02_test
 
 import (
 	"day02"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -11,4 +12,25 @@ func TestHelloWithName(t *testing.T) {
 	h := day02.NewHello("somkiat")
 	got := h.SayHi()
 	assert.Equal(t, "Hello somkiat", got)
+}
+
+func ExampleHello_SayHi() {
+	h := day02.NewHello("somkiat")
+	got := h.SayHi()
+	fmt.Println(got)
+	// Output: Hello somkiat
+}
+
+func BenchmarkV1SayHi(b *testing.B) {
+	h := day02.NewHello("somkiat")
+    for i := 0; i < b.N; i++ {
+		h.SayHi()
+    }
+}
+
+func BenchmarkV2SayHi(b *testing.B) {
+	h := day02.NewHello("somkiat")
+    for i := 0; i < b.N; i++ {
+		h.SayHi()
+    }
 }
