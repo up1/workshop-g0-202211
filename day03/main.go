7d6124bf893eda98c8067efe97e59109a10679ee
up1 <somkiat.p@gmail.com>
2022-11-09 10:16:11 +0700

    Working with panic and recover

--- day03/main.go
+++ day03/main.go
@@ -4,14 +4,21 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/recover"
 )
 
 func main() {
 	app := fiber.New()
+	app.Use(recover.New())
 
 	app.Get("/", hello)
+	app.Get("/panic", withPanic)
 
 	log.Fatal(app.Listen(":3000"))
+}
+
+func withPanic(c *fiber.Ctx) error {
+	panic("Some error")
 }
 
 func hello(c *fiber.Ctx) error {
