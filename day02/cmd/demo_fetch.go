0d1b3af86868d2211c8ce1f692c9ac5af9c9c5ec
up1 <somkiat.p@gmail.com>
2022-11-09 09:02:45 +0700

    Demo fetch

---
+++ day02/cmd/demo_fetch.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type result struct {
+	string
+	bool
+}
+
+func main(){
+	urls := []string{
+		"https://www.google.com",
+		"https://www.somkiat.cc",
+	}
+
+	resultChannel := make(chan result)
+
+	for _, url := range urls {
+		go func(u string) {
+			resultChannel <- result{u, FetchData(u)}
+		}(url)
+	}
+
+	for i := 0; i < len(urls); i++ {
+		r := <-resultChannel
+		fmt.Println(r.string, ":", r.bool)
+	}
+}
+
+func FetchData(url string) bool {
+	response, err := http.Head(url)
+	if err != nil {
+		return false
+	}
+
+	return response.StatusCode == http.StatusOK
+}
