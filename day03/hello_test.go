5a36b318d2727cea5fd7bf8a34fef54d4498da55
up1 <somkiat.p@gmail.com>
2022-11-09 14:30:04 +0700

    Working with stub in test

--- day03/hello_test.go
+++ day03/hello_test.go
@@ -27,3 +27,27 @@ func TestSuccesswithHelloHandler(t *testing.T) {
 	assert.Nil(t, nil, err)
 	assert.Contains(t, string(body), "Hello")
 }
+
+type StubRepository struct{}
+
+func (r StubRepository) GetDataFromDb() string {
+	return "From stub"
+}
+
+func TestSuccesswithHelloHandlerWithStub(t *testing.T) {
+	app := day03.NewFiberRouter()
+	// init dependency
+	r := StubRepository{}
+	s := day03.NewService(r)
+	day03.NewHelloRouter(app, s)
+	// Call Target endpoint
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	res, _ := app.Test(req, -1)
+
+	// Assert status code
+	assert.Equal(t, 200, res.StatusCode)
+	// Assert body
+	body, err := io.ReadAll(res.Body)
+	assert.Nil(t, nil, err)
+	assert.Contains(t, string(body), "From stub")
+}
