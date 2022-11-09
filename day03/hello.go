b1f42fec6aabcb5af699bcc26a93cc3c2737c629
up1 <somkiat.p@gmail.com>
2022-11-09 13:44:59 +0700

    Add service and repository

--- day03/hello.go
+++ day03/hello.go
@@ -5,9 +5,35 @@ import "github.com/gofiber/fiber/v2"
 func NewHelloRouter(app *fiber.App) {
 	app.Get("/", Hello)
 }
-
+// Controller or Handler
 func Hello(c *fiber.Ctx) error {
+	r := helloRepository{}
+	s := NewService(r)
+	msg := s.doSth()
+
 	return c.JSON(fiber.Map{
-		"message": "Hello World",
+		"message": msg,
 	})
 }
+
+// Service
+type helloSevice struct {
+	r helloRepository
+}
+
+func (s helloSevice) doSth() string {
+	return "Hello World"
+}
+
+func NewService(r helloRepository) helloSevice {
+	return helloSevice{r: r}
+}
+
+
+
+// Repository
+type helloRepository struct {
+}
+
+
+
