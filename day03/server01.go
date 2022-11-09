5c399d531decfc4ef56108dcc37c3a47b7a667ef
up1 <somkiat.p@gmail.com>
2022-11-09 09:52:27 +0700

    Demo with dependency injection

--- day03/server01.go
+++ day03/server01.go
@@ -8,16 +8,24 @@ import (
 
 type conn struct{}
 
+type hello struct{
+  c conn
+}
+
+func (h hello) helloHandler3(w http.ResponseWriter, req *http.Request) {
+	fmt.Println(h.c)
+}
+
 func main() {
 
 	// Connect to db
 	c := conn{}
-
-	http.HandleFunc("/hello", helloHandler(c))
+	h := hello {c: c}
+	http.HandleFunc("/hello", h.helloHandler3)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func helloHandler(c conn) func(w http.ResponseWriter, req *http.Request) {
+func helloHandler2(c conn) func(w http.ResponseWriter, req *http.Request) {
 	return func(w http.ResponseWriter, req *http.Request) {
 		fmt.Println(c)
 	}
